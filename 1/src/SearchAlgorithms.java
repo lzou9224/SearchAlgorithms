fd30c52a2aff844523a4bd6282aeee1f125af699
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-01-12 09:06:15 -0500

    :)

--- 1/src/SearchAlgorithms.java
+++ 1/src/SearchAlgorithms.java
@@ -1,23 +1,59 @@
+//Lingli Zou
+//Pd. 2
 
 public class SearchAlgorithms {
 	
 	public static void main(String[] args)
-{
-	int[] test1= {1,2,3,4,5,6,7,8,9,10};
-	int[] test2 = {0,4,5,6,12,15,17,23,29};
+	{
+		int[] test1= {1,2,3,4,5,6,7,8,9,10};
+		int[] test2 = {0,4,5,6,12,15,17,23,29};
 	
-	System.out.println(binSearch(test1, 3));
-	System.out.println(linSearch(test1, 3));
+		System.out.println(binSearch(test1, 3));
+		System.out.println(linSearch(test1, 3));
 	
-	System.out.prinln(binSearch(test2, 23));
-	System.out.println(linSearch(test2, 23));
+		System.out.prinln(binSearch(test2, 23));
+		System.out.println(linSearch(test2, 23));
 	
-	System.out.println(binSearch(test2, 25));
-	System.out.println(linSearch(test2, 25));
+		System.out.println(binSearch(test2, 25));
+		System.out.println(linSearch(test2, 25));
+		
+		System.out.println(binSearch(test1, 10));
+		System.out.println(linSearch(test1, 1));
 	
-	System.out.println(binSearch(test1, 10));
-	System.out.println(linSearch(test1, 1));
+		System.out.println(binSearch(test1, 1));
+	}
 	
-	System.out.println(binSearch(test1, 1));
-}
+	public static int linSearch( int[] nums, int query)
+	{
+		for(int x=0; x<nums.length; x++) 
+		{
+			if(nums[x]==query)
+			{
+				return x;
+			}
+		}
+		return(-1);
+
+	}
+	
+	public static int binSearch( int[] nums, int query)
+	{
+		int L=0;
+		int R= arr.length-1;
+		
+		while(L<= R)
+		{
+			int mid= L+(R-L)/2;
+			
+			if(query==arr[mid])
+			{
+				return mid;
+			}
+			if else(query < arr[mid])
+			{
+				
+			}
+		}
+	}
+	
 }
